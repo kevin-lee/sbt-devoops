eb2b0edba55a6b06f28c1255df884123fd5c7470
Kevin Lee <kevin.code@kevinlee.io>
2019-03-05 00:00:22 +1100

    Better way to generate filenames in test

--- src/test/scala/kevinlee/sbt/io/Gens.scala
+++ src/test/scala/kevinlee/sbt/io/Gens.scala
@@ -1,22 +1,13 @@
 package kevinlee.sbt.io
 
-import java.nio.charset.StandardCharsets
-import java.security.SecureRandom
-
 import hedgehog._
 import kevinlee.test.data.{Content, Names, NamesAndContent}
-
-import scala.util.Random
 
 /**
   * @author Kevin Lee
   * @since 2019-02-23
   */
 object Gens {
-
-  val random: Random =
-    new Random(new SecureRandom("Gens".getBytes(StandardCharsets.UTF_8)))
-
 
   def genFilenamesAndContent: Gen[(List[String], String)] = for {
     names <- Gen.string(Gen.alphaNum, Range.linear(1, 5)).list(Range.linear(1, 5))
@@ -27,11 +18,10 @@ object Gens {
     Gens.genFilenamesAndContent
       .list(Range.linear(1, 5))
       .map { namesList =>
-        namesList.map { case (names, content) =>
-          val randomNumber = random.nextInt()
+        namesList.zipWithIndex.map { case ((names, content), index) =>
           NamesAndContent(
               Names(
-                names.headOption.fold(s"abc_$randomNumber")(x => s"${x}_$randomNumber") :: names.drop(1)
+                names.take(1).map(x => s"${x}_$index") ++ names.drop(1)
               )
             , Content(content)
           )
