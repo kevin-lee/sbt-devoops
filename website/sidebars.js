a7af299200bdfe87acefde905c51f3b4a62241d9
Kevin Lee <kevin.code@kevinlee.io>
2020-07-24 18:46:46 +1000

    Changed: doc site config * The container max width: 1140px => 1240px * gh-release-plugin pages in the sidebar are not collapsed by default * Change and fix in labeling and path

--- website/sidebars.js
+++ website/sidebars.js
@@ -1,8 +1,22 @@
 module.exports = {
-  someSidebar: {
-    'DevOops': ['getting-started'],
-    'DevOopsGitReleasePlugin': ['gh-release-plugin/config-and-run', 'gh-release-plugin/examples'],
-    'DevOopsScalaPlugin': ['scala-plugin/how-to-use'],
-    'DevOopsJavaPlugin': ['java-plugin/how-to-use'],
-  },
+  docs: [
+    {
+      type: 'category',
+      label: 'DevOops',
+      collapsed: false,
+      items: ['getting-started'],
+    },
+    {
+      type: 'category',
+      label: 'DevOopsGitReleasePlugin',
+      collapsed: false,
+      items: ['gh-release-plugin/config-and-run', 'gh-release-plugin/examples']
+    },
+    {
+      'DevOopsScalaPlugin': ['scala-plugin/how-to-use']
+    },
+    {
+      'DevOopsJavaPlugin': ['java-plugin/how-to-use']
+    },
+  ],
 };
