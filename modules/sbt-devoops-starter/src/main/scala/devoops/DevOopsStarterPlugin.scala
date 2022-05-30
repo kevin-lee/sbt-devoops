4978c728e4c7cef8cfd75a5685acb0db2b1a872b
Kevin Lee <kevin.code@kevinlee.io>
2022-05-30 21:26:38 +1000

    Close #375 - Add task to write default `.scalafix.conf` files

--- modules/sbt-devoops-starter/src/main/scala/devoops/DevOopsStarterPlugin.scala
+++ modules/sbt-devoops-starter/src/main/scala/devoops/DevOopsStarterPlugin.scala
@@ -2,18 +2,23 @@ package devoops
 
 import org.scalafmt.sbt.ScalafmtPlugin
 import sbt.Keys._
-import sbt._
+import sbt.{none => _, some => _, _}
 import Def.Setting
 import cats.effect.{Async, IO}
+import cats.syntax.all._
 import devoops.data.{CommonKeys, DevOopsLogLevel}
 import devoops.types.StarterError
+import effectie.cats.fx.ioFx
 import effectie.core.Fx
 import effectie.syntax.all._
 import extras.cats.syntax.all._
 import extras.scala.io.Color
+import extras.scala.io.syntax.color._
 import kevinlee.github.GitHubApi
 import kevinlee.github.data.GitHub
 import kevinlee.http.HttpClient
+import kevinlee.sbt.SbtCommon._
+import loggerf.cats.instances._
 import loggerf.core.{Log => LogF}
 import loggerf.logger.{CanLog, SbtLogger}
 import org.http4s.blaze.client.BlazeClientBuilder
@@ -21,7 +26,6 @@ import sbt.{IO => SbtIO}
 import sbtwelcome.WelcomePlugin
 import sbtwelcome.WelcomePlugin.autoImport.{logo, logoColor, usefulTasks}
 import scalafix.sbt.ScalafixPlugin
-import extras.scala.io.syntax.color._
 
 import java.nio.charset.StandardCharsets
 
@@ -54,7 +58,12 @@ object DevOopsStarterPlugin extends AutoPlugin {
       """The additional information displayed after logo (default: "")"""
     )
 
-    lazy val starterWriteDefaultScalafmtConf: TaskKey[Unit] = taskKey("Write the default .scalafmt.conf")
+    lazy val starterWriteDefaultScalafmtConf: TaskKey[Unit] = taskKey("Write the (default: .scalafmt.conf)")
+
+    lazy val starterWriteDefaultScalafixConf: TaskKey[Unit] = taskKey(
+      "Write the (default .scalafix.conf if the project has only scala 2 or only scala 3. " +
+        "Otherwise .scalafix-scala2.conf and .scalafix-scala3.conf)"
+    )
 
     def genLogo(
       projectName: String,
@@ -84,9 +93,6 @@ object DevOopsStarterPlugin extends AutoPlugin {
       val baseDirFile    = (ThisBuild / baseDirectory).value
       val outFile        = baseDirFile / ".scalafmt.conf"
 
-      import effectie.cats.fx.ioFx
-      import loggerf.cats.instances._
-
       implicit val log: CanLog = SbtLogger.sbtLoggerCanLog(streams.value.log)
       import cats.effect.unsafe.implicits.global
 
@@ -99,12 +105,71 @@ object DevOopsStarterPlugin extends AutoPlugin {
       } else {
         writeDefaultScalafmtConf[IO](dialectVersion, outFile)
           .unsafeRunSync() match {
-          case Left(err) => sys.error(err.render)
+          case Left(err) => messageOnlyException(err.render)
           case Right(_) => log.info(s"The default ${outFile.getName.blue} file has been written.")
         }
       }
 
-    }
+    },
+    starterWriteDefaultScalafixConf := {
+      implicit val devOopsLogLevelValue: DevOopsLogLevel = DevOopsLogLevel.fromStringUnsafe(devOopsLogLevel.value)
+      val crossScalaVers                                 = crossScalaVersions.value
+      val scalaVer                                       = scalaVersion.value
+      val baseDirFile                                    = (ThisBuild / baseDirectory).value
+
+      implicit val log: CanLog = SbtLogger.sbtLoggerCanLog(streams.value.log)
+      import cats.effect.unsafe.implicits.global
+
+      if (crossScalaVers.exists(_.startsWith("2.")) && crossScalaVers.exists(_.startsWith("3."))) {
+        val (source1, out1) = ("default-scalafix-scala2.conf.template", ".scalafix-scala2.conf")
+        val (source2, out2) = ("default-scalafix-scala3.conf.template", ".scalafix-scala3.conf")
+
+        val outFile1 = baseDirFile / out1
+        val outFile2 = baseDirFile / out2
+
+        if (!outFile1.exists() && !outFile2.exists()) {
+          (
+            writeDefaultScalafixConf(source1, outFile1),
+            writeDefaultScalafixConf(source2, outFile2)
+          )
+            .parMapN { (r1, r2) =>
+              (r1.toEitherNec, r2.toEitherNec).parMapN((_, _))
+            }
+            .unsafeRunSync() match {
+            case Left(errs) => messageOnlyException(errs.map(_.render).toList.mkString("[", ",", "]"))
+            case Right((outFile1, outFile2)) =>
+              log.info(s"The default ${outFile1.getName.blue} and ${outFile2.getName.blue} files have been written.")
+          }
+        } else {
+          log.info(
+            raw"""${outFile1.getName.blue} or ${outFile2.getName.blue} or both already exist so it will not overwrite.
+                 |If you want to regenerate them, please remove the existing ones.
+                 |""".stripMargin
+          )
+        }
+      } else {
+        val (source, out) =
+          if (scalaVer.startsWith("2."))
+            ("default-scalafix-scala2.conf.template", ".scalafix.conf")
+          else
+            ("default-scalafix-scala3.conf.template", ".scalafix.conf")
+        val outFile       = baseDirFile / out
+
+        if (outFile.exists()) {
+          log.info(
+            raw"""${outFile.getName.blue} already exists so it will not rewrite ${outFile.getName.blue}.
+                 |If you want to regenerate it, please remove the existing one.
+                 |""".stripMargin
+          )
+        } else {
+          writeDefaultScalafixConf(source, outFile)
+            .unsafeRunSync() match {
+            case Left(err) => messageOnlyException(err.render)
+            case Right(outFile) => log.info(s"The default ${outFile.getName.blue} file has been written.")
+          }
+        }
+      }
+    },
   )
 
   def writeDefaultScalafmtConf[F[_]: Fx: LogF: Async](dialectVersion: String, outFile: File)(
@@ -115,15 +180,15 @@ object DevOopsStarterPlugin extends AutoPlugin {
       .use { client =>
         (for {
           gitHubApi <- GitHubApi[F](HttpClient[F](client)).rightTF
-          tags <- gitHubApi
-                    .getTags(
-                      GitHub.GitHubRepoWithAuth(
-                        GitHub.Repo(GitHub.Repo.Org("scalameta"), GitHub.Repo.Name("scalafmt")),
-                        none
-                      )
-                    )
-                    .t
-                    .leftMap(StarterError.gitHub("getting the tags", _))
+          tags      <- gitHubApi
+                         .getTags(
+                           GitHub.GitHubRepoWithAuth(
+                             GitHub.Repo(GitHub.Repo.Org("scalameta"), GitHub.Repo.Name("scalafmt")),
+                             none
+                           )
+                         )
+                         .t
+                         .leftMap(StarterError.gitHub("getting the tags", _))
           theLatestScalafmtVersion = tags.headOption.fold("3.5.4")(_.name.name.stripPrefix("v"))
           scalafmtConfTemplate <- effectOf[F](
                                     SbtIO.readStream(
@@ -152,6 +217,32 @@ object DevOopsStarterPlugin extends AutoPlugin {
                                   }.t
         } yield ()).value
       }
+
+  def writeDefaultScalafixConf[F[_]: Fx: LogF: Async](templateFilename: String, outFile: File)(
+    implicit LV: DevOopsLogLevel
+  ): F[Either[StarterError, File]] =
+    (for {
+      scalafixConfTemplate <- effectOf[F](
+                                SbtIO.readStream(
+                                  this.getClass.getResourceAsStream(s"/scalafix/$templateFilename"),
+                                  StandardCharsets.UTF_8
+                                )
+                              ).catchNonFatal { err =>
+                                StarterError.resourceReadWrite(
+                                  s"reading the $templateFilename file at resources/scalafix/$templateFilename",
+                                  err.toString
+                                )
+
+                              }.t
+      _                    <- effectOf[F](
+                                SbtIO.write(outFile, scalafixConfTemplate, StandardCharsets.UTF_8)
+                              ).catchNonFatal { err =>
+                                StarterError.resourceReadWrite(
+                                  s"writing the default scalafix config file to ${outFile.toString}",
+                                  err.toString
+                                )
+                              }.t
+    } yield outFile).value
 
   override def projectSettings: Seq[Setting[_]] = Seq(
     theLogo            := DefaultLogo,
